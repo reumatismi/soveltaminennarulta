d58e0dfa070de37eeb4d7ab2c0da8fa9659331c5
keenanjon <jon.menna@metropolia.fi>
2021-05-07 10:45:02 +0300

    delete comment fixed

--- back/public/js/main4.js
+++ back/public/js/main4.js
@@ -1,6 +1,6 @@
 'use strict';
-const url = 'https://localhost:8000'; // change url when uploading to server
-//const url = 'https://10.114.32.88/app'; // change url when uploading to server
+//const url = 'https://localhost:8000'; // change url when uploading to server
+const url = 'https://10.114.32.88/app'; // change url when uploading to server
 
 // select existing html elements
 const helloAntero = document.querySelector('#helloSirpa');
@@ -242,6 +242,7 @@ const createMediaCards = (mediaPosts, comments) => {
             getMediaPosts();
             console.log('Error in deleye comment',e);
           }
+          getMediaPosts();
         });
         commentPost.appendChild(commentDelete);
         commentContainer.appendChild(commentPost);
